e94316710ce0b65f1b362db3023f38306a726ce5
Anastacia Wainaina <37904262+Vicky-AnnaStacy@users.noreply.github.com>
2023-01-31 07:29:44 +0300

    services js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-(document).ready(function(){
-    $("#development-image").click(function(){
-      $("#development-image").hide();
-      $("#development").show();
+
+  $(document).ready(function () {
+    /*Form section*/
+    $("form#form34A").submit(function (event) {
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()) {
+        alert(
+          name +
+            ", we have received your message. Thank you for reaching out to us."
+        );
+      } else {
+        alert("Please enter your name and email!");
+      }
     });
-    $("#development").click(function(){
-      $("#development").hide();
-      $("#development-image").show();
+    /*What we do section*/
+    $("#whole-design").click(function () {
+      $("#design").toggle();
+      $("#design-image").toggle();
     });
-  });
+  
