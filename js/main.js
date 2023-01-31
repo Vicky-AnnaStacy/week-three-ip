714f68ccc5639263d3b08b4d2a56dd02316a5380
Anastacia Wainaina <37904262+Vicky-AnnaStacy@users.noreply.github.com>
2023-01-31 08:15:13 +0300

    responsiveness

--- js/main.js
+++ js/main.js
@@ -14,8 +14,164 @@
       }
     });
     /*What we do section*/
-    $("#whole-design").click(function () {
-      $("#design").toggle();
+    $("#whole-design").click(function () ); {
+
+    }
+
+      $"#design").toggle();
       $("#design-image").toggle();
+    
+      $(document).ready(function(){
+        /*Form section*/
+        $("form#form34A").submit(function(event){
+          // event.preventDefault();
+          var name = $("input#MERGE1").val();
+          if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+            alert (name + ", we have received your message. Thank you for reaching out to us.");
+          }
+          else {
+            alert("Please enter your name and email!");
+          }
+    
+        });
+    /*What we do section*/
+        $("#whole-design").click(function(){
+            $("#design").toggle();
+            $("#design-image").toggle();
+        });
+      $("#whole-development").click(function () {
+        $("#development").toggle();
+        $("#developmet-image").toggle();
+      });
+      $("#whole-product").click(function () {
+        $("#product").toggle();
+        $("#product-image").toggle();
+      });
+    
+        $("#whole-development").click(function(){
+            $("#development").toggle();
+            $("#developmet-image").toggle();
+        });
+        $("#whole-product").click(function(){
+            $("#product").toggle();
+            $("#product-image").toggle();
+      //Insert white box upo hover
     });
-  
+    $(document).ready(function () {
+      $("#work1")
+        .mouseover(function () {
+          $("#overlay").show();
+        })
+        .mouseout(function () {
+          $("#overlay").hide();
+        });
+    
+          //Insert white box upo hover
+    
+    
+      });
+      $(document).ready(function () {
+        $("#work1").mouseover(function () {
+            $("#overlay").show();
+        }).mouseout(function () {
+            $("#overlay").hide();
+        });
+    });
+    $(document).ready(function () {
+        $("#work2").mouseover(function () {
+            $("#overlay2").show();
+        }).mouseout(function () {
+            $("#overlay2").hide();
+        });
+      $("#work2")
+        .mouseover(function () {
+          $("#overlay2").show();
+        })
+        .mouseout(function () {
+          $("#overlay2").hide();
+        });
+    });
+    $(document).ready(function () {
+        $("#work3").mouseover(function () {
+            $("#overlay3").show();
+        }).mouseout(function () {
+            $("#overlay3").hide();
+        });
+      $("#work3")
+        .mouseover(function () {
+          $("#overlay3").show();
+        })
+        .mouseout(function () {
+          $("#overlay3").hide();
+        });
+    });
+    $(document).ready(function () {
+        $("#work4").mouseover(function () {
+            $("#overlay4").show();
+        }).mouseout(function () {
+            $("#overlay4").hide();
+        });
+      $("#work4")
+        .mouseover(function () {
+          $("#overlay4").show();
+        })
+        .mouseout(function () {
+          $("#overlay4").hide();
+        });
+    });
+    
+    $(document).ready(function () {
+        $("#work5").mouseover(function () {
+            $("#overlay5").show();
+        }).mouseout(function () {
+            $("#overlay5").hide();
+        });
+        $("#work6").mouseover(function () {
+            $("#overlay6").show();
+        }).mouseout(function () {
+            $("#overlay6").hide();
+        });
+        $("#work7").mouseover(function () {
+            $("#overlay7").show();
+        }).mouseout(function () {
+            $("#overlay7").hide();
+        });
+        $("#work8").mouseover(function () {
+            $("#overlay8").show();
+        }).mouseout(function () {
+            $("#overlay8").hide();
+        });
+    });
+      $("#work5")
+        .mouseover(function () {
+          $("#overlay5").show();
+        })
+        .mouseout(function () {
+          $("#overlay5").hide();
+        });
+      $("#work6")
+        .mouseover(function () {
+          $("#overlay6").show();
+        })
+        .mouseout(function () {
+          $("#overlay6").hide();
+        });
+      $("#work7")
+        .mouseover(function () {
+          $("#overlay7").show();
+        })
+        $("#work7")
+        .mouseover(function () {
+          $("#overlay7").show();
+        })
+        .mouseout(function () {
+          $("#overlay7").hide();
+        });
+      $("#work8")
+        .mouseover(function () {
+          $("#overlay8").show();
+        })
+        .mouseout(function () {
+          $("#overlay8").hide();
+        });
+    });
